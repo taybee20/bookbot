32a47de965300c6ea6fdd85b1445fae7748f751e
taybee20 <taybee20@gmail.com>
2024-12-06 14:37:59 -0800

    Finished character_count method

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = wordcount(text)
+    char_list = character_count(text)
 
-    print(word_count)
+    
+   
+    # Displaying report
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print(char_list)
 
-
-
-# Takes text string from book and returns integer number of words in string
+# Takes text string (a book) and returns the number of words in string
 def wordcount(book):
     words = book.split()
 
@@ -19,6 +23,19 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+# Takes text string (a book) and returns the number of times each character appears in the string 
+def character_count(book):
+    characters = {}
+    
+    for c in book: 
+        char = c.lower()
+
+        if char in characters:
+           characters[char] += 1
+        else:
+            characters[char] = 1
+
+    return characters
 
 if __name__ == "__main__":
     main()
